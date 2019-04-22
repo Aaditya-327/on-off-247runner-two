b17a58d30d88eec5f1b0ea92a01b2821a38c82aa
Aaditya Bhattarai <30322868+Aaditya-327@users.noreply.github.com>
2019-04-22 14:57:19 +0545

    Add files via upload

---
+++ Procfile
@@ -0,0 +1,1 @@
+worker: python s2.py
---
+++ s2.py
@@ -0,0 +1,47 @@
+#for on-off-247runner-two
+
+first = ["on-off-247runner-one", ]
+second = ["on-off-247runner-two", ]
+
+#---------------------------------------------------------------
+
+import heroku3
+import time
+import os
+
+# gapOfCheck = 1728000
+gapOfCheck = 300
+HEROKU_API = os.environ['HEROKU_API']
+heroku_conn = heroku3.from_key(HEROKU_API)
+
+#---------------------------------------------------------------
+def findMyApp(apps, appName):
+    for _ in apps: 
+        if _.name == appName: 
+            return _
+
+def enableApps(applist, appListText):
+    for _ in appListText:
+        print("A")
+        findMyApp(applist, _).enable_maintenance_mode()
+
+def disableApps(applist, appListText):
+    for _ in appListText:
+        print("B")
+        findMyApp(applist, _).disable_maintenance_mode()
+
+#---------------------------------------------------------------
+last_time = time.time()%gapOfCheck
+while True:
+    time.sleep(1)
+    now = time.time()%gapOfCheck
+
+    print("Hands changed from B to A")
+    # print(last_time, now, now-last_time)
+    if last_time< gapOfCheck and now> 0 and now-last_time<0:
+        if last_time-now>gapOfCheck/4:  
+            apps = heroku_conn.apps()   
+            enableApps(apps, first)
+            disableApps(apps, second)
+            
+    last_time = now
